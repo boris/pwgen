c9df567fd6ec6d83be6795bd5143127cd5df7da3
Boris Quiroz <boris@insert-coin.org>
2017-06-18 03:01:05 -0400

    Adding a new route

--- app.py
+++ app.py
@@ -4,13 +4,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+passwds = []
+
 @app.route('/')
 def hello_world():
     return render_template('index.html')
 
+@app.route('/<int:length>')
+def default_usage(length):
+    return pwgen(length) + "\n"
+
 @app.route('/<int:length>/<int:count>')
 def generate_random(length, count):
-    return "\n".join(pwgen(length, count)) + "\n"
+    passwds.append(pwgen(length, count))
+    return passwds
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
