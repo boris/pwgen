8d8aee7ee668edb26ae3e15e2462a349f9a91b5d
Boris Quiroz <boris@insert-coin.org>
2017-06-18 02:10:10 -0400

    rename app

---
+++ app.py
@@ -0,0 +1,16 @@
+from pwgen import pwgen
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/<int:length>/<int:count>')
+def generate_random(length, count):
+    return "\n".join(pwgen(length, count)) + "\n"
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0')
