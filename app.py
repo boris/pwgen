fc9c61f4e44b0db36e172601a826c7eb0837db93
Boris Quiroz <boris@insert-coin.org>
2018-02-22 16:42:55 -0300

    Add route to generate vault token ids

--- app.py
+++ app.py
@@ -19,6 +19,16 @@ def generate_random(length, count):
     else:
         return "\n".join(pwgen(length, count, symbols=True, allowed_symbols="!@#$%^&*()_-=+,.<>/?;:{}[]\|")) + "\n"
 
+@app.route('/vault')
+def generate_id():
+    a = pwgen(8, symbols=False)
+    b = pwgen(4, symbols=False)
+    c = pwgen(4, symbols=False)
+    d = pwgen(4, symbols=False)
+    e = pwgen(12, symbols=False)
+
+    return a.lower + "-" + b + "-" + c + "-" + d + "-" + e + "\n"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
