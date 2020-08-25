d6dc1415f32bcc9fc59dd09909e7e70020c14330
Boris Quiroz <boris@insert-coin.org>
2020-08-25 10:32:34 -0400

    Add prometheus import and export some counts

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from pwgen import pwgen
 from flask import Flask
 from flask import render_template
 from flask import Response
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+from prometheus_client import make_wsgi_app, Counter, Gauge
 
 app = Flask(__name__)
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+    })
+
+single_count = Counter('single_requests', 'Requests of single passwd')
+multi_count = Counter('multi_requests', 'Requests of multiple passwds')
 
 @app.route('/')
 def hello_world():
@@ -16,15 +24,18 @@ def healthz():
 @app.route('/<int:length>')
 def default_usage(length):
     passwd = pwgen(length, symbols=True, allowed_symbols="!@#$%^&*()_-=+,.<>/?;:{}[]\|") + "\n"
+    single_count.inc()
     return Response(passwd, mimetype="text/plain")
 
 @app.route('/<int:length>/<int:count>')
 def generate_random(length, count):
     if count == 1:
         passwd = pwgen(length, symbols=True, allowed_symbols="!@#$%^&*()_-=+,.<>/?;:{}[]\|") + "\n"
+        single_count.inc()
         return Response(passwd, mimetype="text/plain")
     else:
         passwd = "\n".join(pwgen(length, count, symbols=True, allowed_symbols="!@#$%^&*()_-=+,.<>/?;:{}[]\|")) + "\n"
+        multi_count.inc()
         return Response(passwd, mimetype="text/plain")
 
 @app.route('/donate')
