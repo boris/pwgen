9a7088077fccfe76c5c7f434d7a842261dce652c
Boris Quiroz <boris@insert-coin.org>
2021-08-30 08:01:43 -0400

    Lambda permission from API GW and output API GW url

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -33,3 +33,11 @@ resource "aws_lambda_function" "pwgen_py" {
 
   role = aws_iam_role.lambda_exec.arn
 }
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.pwgen_py.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.pwgen.execution_arn}/*/*"
+}
--- terraform/output.tf
+++ terraform/output.tf
@@ -7,3 +7,8 @@ output "s3_deployment_bucket" {
   description = "Name of the S3 deployment bucket."
   value       = aws_s3_bucket.s3_pwgen.id
 }
+
+output "base_url" {
+  description = "Base URL for API Gateway stage."
+  value       = aws_apigatewayv2_stage.pwgen.invoke_url
+}
