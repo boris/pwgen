743dc800e2040c258a3519852e301efa3d1a49e4
Boris Quiroz <boris@insert-coin.org>
2021-08-30 08:01:29 -0400

    APIGW and logs for it

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -3,3 +3,9 @@ resource "aws_cloudwatch_log_group" "pwgen_lambda" {
 
   retention_in_days = 7
 }
+
+resource "aws_cloudwatch_log_group" "api_gw" {
+  name = "/aws/api_gw/${aws_apigatewayv2_api.pwgen.name}"
+
+  retention_in_days = 7
+}
