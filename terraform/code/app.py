21f35e97c7d9e51906bd604f876222ba8102d292
Boris Quiroz <boris@insert-coin.org>
2021-08-30 08:00:57 -0400

    Fix code to allow API GW

--- terraform/code/app.py
+++ terraform/code/app.py
@@ -1,8 +1,15 @@
 from pwgen import pwgen
+import json
 
 def lambda_handler(event, context):
     passwd = pwgen(24,
             symbols=True,
             )
-    return passwd
+    return {
+            'statusCode': 200,
+            'headers': { 'Content-Type': 'application/json'  },
+            'body': json.dumps({
+                'value': passwd
+                })
+            }
 
