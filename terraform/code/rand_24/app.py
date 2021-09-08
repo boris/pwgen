5abc71637ddc044f5d6a484b2a233a5c0b7e993a
Boris Quiroz <boris@insert-coin.org>
2021-09-08 07:54:32 -0300

    allow arbitraty values

--- terraform/code/rand_24/app.py
+++ terraform/code/rand_24/app.py
@@ -2,22 +2,31 @@ from pwgen import pwgen
 import json
 
 def lambda_handler(event, context):
-    passwd = pwgen(42,
-            symbols=False,
-            )
+    length = event['pathParameters']['proxy'].split('/')[0]
+    count = event['pathParameters']['proxy'].split('/')[1]
+
+    if not count:
+        passwd = pwgen(int(length),
+                symbols=False,
+                )
+    else:
+        passwd = pwgen(int(length),
+                int(count),
+                symbols=False,
+                )
     return {
             'statusCode': 200,
             'headers': { 'Content-Type': 'application/json'  },
             'body': json.dumps({
                 'value': passwd,
-                'path': event,
+                'path': event['pathParameters'],
                 })
             }
 
 def hello_world(event, context):
     return {
             'statusCode': 200,
-            'headers': { 'Content-Type': 'application/json'},
+            'headers': { 'Content-Type': 'application/text'},
             'body': json.dumps({
                'message' : 'hello world!'
                })
